0a69ba84a4979b8f84b458b68f81c7919633bbd0
Mayur Khomane <mayurkhomane01@gmail.com>
2018-12-11 21:14:55 +0530

    Correction to name error

--- L1_2_code_check.py
+++ L1_2_code_check.py
@@ -2,5 +2,5 @@ testProc([1, 2, 3]) # Explicitly passing in a list
 testProc() # Using a default empty list
 def testProc(n = []):
 # Do something with n
-print n     '''Error: syntax error in python3 n should be enclosed in brackets and exception error(NameException)
-               n is local to function def testProc and it cannot be accessed outside of it.'''
+print n     '''Error: syntax error in python3 n should be enclosed in brackets and 
+               NameError, n is local to function testProc and it cannot be accessed outside of it.'''
